3d7a35534e9ec6fd123627c5643ea380db6d4899
Max Tsepkov <max@garygolden.me>
2013-04-01 20:54:57 +0600

    status form input validation

--- src/Form/StatusForm.php
+++ src/Form/StatusForm.php
@@ -2,11 +2,14 @@
 
 namespace ggPerfectmoney\Form;
 
+use Zend\InputFilter;
+use Zend\Validator;
+
 /**
  * This form may be displayed to enable debugging.
  *
  */
-class StatusForm extends PaymentForm
+class StatusForm extends PaymentForm implements InputFilter\InputFilterProviderInterface
 {
     public function __construct($name = null, $options = array())
     {
@@ -14,6 +17,12 @@ class StatusForm extends PaymentForm
 
         $this->setAttribute('action', $options['payment']['status_url']);
 
+        $this->add(array(
+            'name' => 'PAYER_ACCOUNT',
+            'attributes' => array(
+                'value' => 'U' . mt_rand()
+            )
+        ));
         $this->add(array(
             'name' => 'PAYMENT_ID',
             'attributes' => array(
@@ -23,13 +32,19 @@ class StatusForm extends PaymentForm
         $this->add(array(
             'name' => 'PAYMENT_BATCH_NUM',
             'attributes' => array(
-                'value' => mt_rand(1, PHP_INT_MAX)
+                'value' => mt_rand()
             )
         ));
         $this->add(array(
             'name' => 'TIMESTAMPGMT',
             'attributes' => array(
                 'value' => time()
+            )
+        ));
+        $this->add(array(
+            'name' => 'V2_HASH',
+            'attributes' => array(
+                'value' => strtoupper(md5(mt_rand()))
             )
         ));
 
@@ -42,5 +57,53 @@ class StatusForm extends PaymentForm
                 $element->setLabel($element->getName());
             }
         }
+
+        // set amount for testing
+        $this->get('PAYMENT_AMOUNT')->setValue(100);
+    }
+
+    public function getInputFilterSpecification()
+    {
+        return array(
+            'PAYEE_ACCOUNT' => array(
+                'required' => true,
+                'validators' => array(
+                    new Validator\Identical($this->options['merchant']['UAccount'])
+                )
+            ),
+            'PAYMENT_AMOUNT' => array(
+                'required' => true,
+                'validators' => array(
+                    new Validator\GreaterThan(0)
+                )
+            ),
+            'V2_HASH' => array(
+                'required' => true,
+                'validators' => array(
+                    new Validator\Callback(array(
+                        'callback' => function ($value, $context, $key) {
+                            return $value === static::generateHash($context, $key);
+                        },
+                        'callbackOptions' => $this->options['merchant']['apiKey']
+                    ))
+                )
+            )
+        );
+    }
+
+    public static function generateHash($values, $key)
+    {
+        $hash = md5(implode(':', array(
+            $values['PAYMENT_ID'],
+            $values['PAYEE_ACCOUNT'],
+            $values['PAYMENT_AMOUNT'],
+            $values['PAYMENT_UNITS'],
+            $values['PAYMENT_BATCH_NUM'],
+            $values['PAYER_ACCOUNT'],
+            $key,
+            $values['TIMESTAMPGMT']
+        )));
+
+        return strtoupper($hash);
     }
 }
